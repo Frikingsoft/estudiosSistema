123b4815d38636b90d5a37f5250749117d1fdeff
frikingsoft <nicolas.barone1@gmail.com>
2026-05-23 06:03:31 +0200

    respaldo

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="escritorio">
+    <Menu/>
+  </div>
+</template>
+<script setup>
+  import Menu from "./components/Menu.vue"
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow: hidden;
+
+  background-image: url("./fondo.png");
+  background-size:100% 100vh ;
+  
+
+  font-family: sans-serif;
+}
+
+
+
+
+
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,134 @@
+<template>
+     <div class="menu">
+      <nav class="botones_menu">
+        <button class="botones">
+          🏠
+        </button>
+
+        <button class="botones">
+          🌐
+        </button>
+
+        <button class="botones">
+          📁
+        </button>
+
+        <button class="botones">
+          ⚙️
+        </button>
+        </nav>
+    </div>
+</template>
+<style scoped>
+    .escritorio {
+  width: 100%;
+  height: 100vh;
+}
+
+/* ========================= */
+/* MENU INFERIOR */
+/* ========================= */
+
+.menu {
+  position: absolute;
+  bottom: 20px;
+
+  width: 100%;
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* CONTENEDOR DE BOTONES */
+
+.botones_menu {
+  height: 100%;
+
+  width: 25%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 12px;
+
+  padding: 8px 14px;
+
+  border-radius: 18px;
+
+  background: rgba(55, 6, 77, 0.25);
+
+  backdrop-filter: blur(12px);
+
+  box-shadow:
+    1px 1px 20px rgba(0, 0, 0, 0.35);
+}
+
+
+
+.botones {
+  position: relative;
+
+  width: 50px;
+  height: 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 30px;
+  color: white;
+
+  background: transparent;
+
+  border: none;
+
+  cursor: pointer;
+
+  overflow: hidden;
+
+
+}
+.botones:hover{
+  transition: .8s;
+  transform: scale(1.5);
+  animation: animar_boton .5s infinite;
+
+  border-radius: 50%;
+  
+  
+}
+@keyframes animar_boton {
+   0%{
+      transform:  translateY(1) ;
+     
+   }
+   10%{
+      transform:  translateY(4px) ;
+   
+   }
+    20%{
+      transform:  translateY(8px) ;
+       
+   }
+   40%{
+      transform:  translateY(16px) rotateX(-45deg);
+      
+      
+   }
+   50%{
+    transform:  translateY(-4px) ;
+     
+   }
+   60%{
+    transform:  translateY(-8px) ;
+  
+   }
+   100%{
+    transform:  translateY(-16)rotateX(45deg);
+    
+   }
+}
+</style>
